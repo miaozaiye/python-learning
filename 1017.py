ca3f13512ab3e79e964ba7c20c578cb7cc6d5831
Jiang Lu <fevberkeley@vip.sina.com>
2015-10-17 22:49:02 +0800

    slots

---
+++ 1017.py
@@ -0,0 +1,16 @@
+class Student(object):
+	pass
+
+s = Student()
+s.name ='Michael' 
+print (s.name)
+
+
+
+def set_age(self,age):
+	self.age = age
+
+from types import MethodType
+s.set_age = MethodType(set_age,s)
+s.set_age(25)
+print s.age
