c8301c0a3876d0b354e8c0ac64839bbbaf07d789
Jiang Lu <fevberkeley@vip.sina.com>
2015-10-25 23:48:23 +0800

    error & debug

---
+++ 1025.py
@@ -0,0 +1,30 @@
+# err_logging.py
+
+import logging
+
+def foo(s):
+	return 10/int(s)
+
+def bar(s):
+	return foo(s)*2
+
+def main():
+	try:
+		bar('0')
+	except Exception as e:
+		logging.exception(e)
+
+main()
+print ('END')
+
+#error_raise.py
+class FooError(ValueError):
+	pass
+
+def foo(s):
+	n = int(s)
+	if n==0:
+		raise FooError('invalid value: %s' %s)
+	return 10/n
+
+foo('0')
